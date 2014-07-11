cf55b8756c7cc54da6de3165ada73d3e48565cfc
Ben Schwarz <schwarzfunk@gmail.com>
2014-07-11 13:01:22 +0200

    Plan algorithm

--- midiwanne.ino
+++ midiwanne.ino
@@ -9,6 +9,13 @@ void setup() {
 }
 
 void loop() {
+    // TODO Go through all input pin groups
+
+    // TODO Identify (physically) topmost closed circuit (if any)
+
+    // TODO Send a MIDI note of the corresponding pitch/velocity for as long as it is closed
+
+    // DEBUG Send a note to check MIDI output
     if (true) { // TODO check real switch
         MIDI.sendNoteOn(42, 127, 1);
         delay(DELAY);
