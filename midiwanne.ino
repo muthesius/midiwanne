ad58e9ee3a286efc7c8f6a39e6eabc66c9da2084
Ben Schwarz <schwarzfunk@gmail.com>
2014-07-11 13:23:17 +0200

    Add input pin initalization

--- midiwanne.ino
+++ midiwanne.ino
@@ -2,7 +2,19 @@
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
+// Pin numbers for sensor steps, grouped per sensor
+int inputPins[][3] = {{2, 3, 4}, {5, 6, 7}, {8, 9, 10}};
+
 void setup() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            // Declare sensor steps as input
+            pinMode(inputPins[i][j], INPUT);
+            // Enable pull-up resistors
+            digitalWrite(inputPins[i][j], HIGH);
+        }
+    }
+
     MIDI.begin();
 }
 
